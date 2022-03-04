a931b5b7f4e2c3d7f0bbc835dc83fb0fb63ccf16
PoojaSolankar <poojasolankar09gmail.com>
2022-03-04 23:06:05 +0530

    Legth of line

--- Linecomp.java
+++ Linecomp.java
@@ -5,6 +5,10 @@ public class Linecomp {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Welcome to line comparison computation program");
-			
+		Integer x1=1, x2=2, y1=1, y2=4;
+		Double AB;
+		AB = Math.sqrt((x2-x1)^2+(y2-y1)^2);
+		System.out.println("Length of line AB :" +AB);
+		
 	}
 }
