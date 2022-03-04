0d429fd2338ac254cab583232df13d2cb07b519b
PoojaSolankar <poojasolankar09gmail.com>
2022-03-05 00:00:38 +0530

    Compare line

--- Linecomp.java
+++ Linecomp.java
@@ -5,7 +5,7 @@ public class Linecomp {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Welcome to line comparison computation program");
-		Integer x1=2, x2=6, y1=4, y2=5;
+		Integer x1=2, x2=6, y1=5, y2=4;
 		Integer c1=2, c2=6, d1=4, d2=5;
 		Double AB, CD;
 		
@@ -15,7 +15,10 @@ public class Linecomp {
 		CD = Math.sqrt((c2-c1)^2+(d2-d1)^2);
 		System.out.println("Length of line CD :" +CD);
 		
+		System.out.println("Check two lines are equal or not :");
 		System.out.println(AB.equals(CD));
 		
+		System.out.println("Compare two Lines :");
+		System.out.println(AB.compareTo(CD));
 	}
 }
