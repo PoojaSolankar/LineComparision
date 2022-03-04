5979b35df11e32c7f2d3556cfec01eaa0d38987f
PoojaSolankar <poojasolankar09gmail.com>
2022-03-04 22:42:25 +0530

    Welcome to line comaparision

---
+++ Linecomp.java
@@ -0,0 +1,10 @@
+package com.Bridgelabz;
+
+public class Linecomp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to line comparison computation program");
+			
+	}
+}
